928dbdd9d314ba9570ac0c38c44a99748ca2596e
Abhishek-Kathayat <abhishekkathayat25@gmail.com>
2019-06-12 18:03:10 +0530

    Added Location endpoint

--- iirssurveyapp/views.py
+++ iirssurveyapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import JsonResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Hello World")
+
+def getlatitude(request, latitude, longitude):
+    location = {
+        'latitude' : latitude,
+        'longitude' : longitude
+    }
+    return JsonResponse(location)
--- iirssurveybackend/settings.py
+++ iirssurveybackend/settings.py
@@ -34,13 +34,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'iirssurveyapp'
+    'django.contrib.gis',
+    'iirssurveyapp.apps.IirssurveyappConfig'
 ]
 
 MIDDLEWARE = [
