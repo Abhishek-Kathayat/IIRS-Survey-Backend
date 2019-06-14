0a38015c03b9b21dd4ddb6a404bb6d409099ccbb
Abhishek-Kathayat <abhishekkathayat25@gmail.com>
2019-06-14 06:24:24 +0530

    View Changes

--- iirssurveyapp/views.py
+++ iirssurveyapp/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.http import JsonResponse
 from django.contrib.gis.geos import GEOSGeometry
 from .models import Userloclayer
-
-import json
+import fiona
+from shapely.geometry import MultiPolygon, mapping, Point, Polygon, shape
+from shapely.geometry.polygon import Polygon
 
 # Create your views here.
 
@@ -12,10 +12,15 @@ def index(request):
     return HttpResponse("IIRS Survey App")
 
 def getlocation(request, latitude, longitude, layer):
+    shphandle = ([ward for ward in fiona.open(r"C:\Users\abhis\Documents\IIRS Internship\Jupyter Lab\DDN_Geo.shp")])
     point = {
         "type": "Point",
         "coordinates": [float(longitude), float(latitude)]
     }
     loclayer = Userloclayer.objects.create(location = GEOSGeometry(json.dumps(point)), layer = layer)
     loclayer.save()
-    return HttpResponse("Values Inserted")
+    for index, ward in enumerate(shphandle):
+        location = Point(float(longitude), float(latitude))
+        if location.within(shape(ward['geometry'])):
+            return HttpResponse(ward['properties']['Name'])
+    return HttpResponse("Location not inside any Ward")
